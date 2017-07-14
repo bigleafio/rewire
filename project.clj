5a114582a7021bcd833df657b3305a5407a34510
Jason S Graham <jgraham20@gmail.com>
2017-07-13 20:42:06 -0500

    Fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rewire "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tinkering with BCV parser to learn Clojure"
+  :url "https://github.com/bigleafio/rewire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
---
+++ src/clj/rewire/core.clj
@@ -0,0 +1,17 @@
+(ns rewire.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(def books ["Gen" "Exod" "Lev" "Num" "Deut" "Josh" "Judg" "Ruth" "1Sam" "2Sam"
+						"1Kgs" "2Kgs" "1Chr" "2Chr" "Ezra" "Neh" "Esth" "Job" "Ps" "Prov"
+						"Eccl" "Song" "Isa" "Jer" "Lam" "Ezek" "Dan" "Hos" "Joel" "Amos"
+						"Obad" "Jonah" "Mic" "Nah" "Hab" "Zeph" "Hag" "Zech" "Mal" "Matt"
+						"Mark" "Luke" "John" "Acts" "Rom" "1Cor" "2Cor" "Gal" "Eph" "Phil"
+						"Col" "1Thess" "2Thess" "1Tim" "2Tim" "Titus" "Phlm" "Heb" "Jas"
+						"1Pet" "2Pet" "1John" "2John" "3John" "Jude" "Rev" "Tob" "Jdt"
+						"GkEsth" "Wis" "Sir" "Bar" "PrAzar" "Sus" "Bel" "SgThree" "EpJer"
+						"1Macc" "2Macc" "3Macc" "4Macc" "1Esd" "2Esd" "PrMan"])
