7b5279d11843533b83ef9b05b953cc03a238032e
Jason S Graham <jgraham20@gmail.com>
2017-07-17 10:10:18 -0500

    Work

--- src/rewire/core.clj
+++ src/rewire/core.clj
@@ -15,3 +15,10 @@
 						"1Pet" "2Pet" "1John" "2John" "3John" "Jude" "Rev" "Tob" "Jdt"
 						"GkEsth" "Wis" "Sir" "Bar" "PrAzar" "Sus" "Bel" "SgThree" "EpJer"
 						"1Macc" "2Macc" "3Macc" "4Macc" "1Esd" "2Esd" "PrMan"])
+
+(defrecord Book "Gen" 1 [31, 25, 24, 26, 32, 22, 24, 22, 29,
+						 32, 32, 20, 18, 24, 21, 16, 27, 33,
+						 38, 18, 34, 24, 20, 67, 34, 35, 46,
+						 22, 35, 43, 55, 32, 20, 31, 29, 43,
+						 36, 30, 23, 23, 57, 38, 34, 34, 28,
+						 34, 31, 22, 33, 26])
